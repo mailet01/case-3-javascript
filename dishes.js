edd291e807e0c0d0f6a99324f667a5abd6ab9459
mailet01 <111491293+mailet01@users.noreply.github.com>
2022-11-29 15:51:09 +0100

    knapp

--- dishes.js
+++ dishes.js
@@ -1,14 +1,29 @@
-const form = document.getElementById("dishes")
-const inputDish = document.getElementById("dishes")
-const addDish = document.getElementById("dishes")
-addDish.addEventListener("click"), 
-function addDish(evt) {
-    console.log(inputDish.value);
-    // lägger till lista i ul-elementet
-    let ul = document.createElement("ul");
-    ul.innerText = inputDish.value;
+function addDish (evt) { 
+
+evt.preventDefault();
+console.log("Emil")
+
+const inputText = document.getElementById("dishes");
+const button = document.getElementById("adddish");
+const AddDish = document.querySelector("#addDish");
+addDish.addeventlist("click")
+console.log(inputText.valu)
+const formEl = document.getElementById("dishes form")
+
+
+
+
+
+
+
+
+
+    
+
+    
+    
     
     
     
         
-}
+
