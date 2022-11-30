f77293eddbefcc1913564d674e8e9cd2234572b4
mailet01 <111491293+mailet01@users.noreply.github.com>
2022-11-30 20:27:26 +0100

    Lägga till li i ul

--- dishes.js
+++ dishes.js
@@ -1,7 +1,5 @@
-function addDish () {
 const ul = document.getElementById("dishes");
 const inputDish = document.getElementById("dishes");
-const addDish = document.querySelector("#adddish");
-addDish.addEventlistener("click", addDish);
-
-}
+const = addDish = document.querySelector("#adddish");
+addDish.addEventListener("click"), addDish;
+let ul = document.createElement("li");
