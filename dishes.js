2febd7e4b999aa4327f80bd7ec32fe4548a90ed3
mailet01 <111491293+mailet01@users.noreply.github.com>
2022-11-30 16:20:58 +0100

    Lielementändrat id

--- dishes.js
+++ dishes.js
@@ -1,29 +1,7 @@
-function addDish (evt) { 
+function addDish () {
+const ul = document.getElementById("dishes");
+const inputDish = document.getElementById("dishes");
+const addDish = document.querySelector("#adddish");
+addDish.addEventlistener("click", addDish);
 
-evt.preventDefault();
-console.log("Emil")
-
-const inputText = document.getElementById("dishes");
-const button = document.getElementById("adddish");
-const AddDish = document.querySelector("#addDish");
-addDish.addeventlist("click")
-console.log(inputText.valu)
-const formEl = document.getElementById("dishes form")
-
-
-
-
-
-
-
-
-
-    
-
-    
-    
-    
-    
-    
-        
-
+}
