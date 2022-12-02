a786ae223f7ad9da7f2df3db1050cb0d197e1217
mailet01 <111491293+mailet01@users.noreply.github.com>
2022-12-02 11:19:21 +0100

    innerHTML

--- dishes.js
+++ dishes.js
@@ -1,11 +1,16 @@
 function addDish(evt) {
-console.log("ghello");
-inputDishEl.value
-console.log(inputDishEl.value);
+    console.log("hello");
+    inputDishEl.value;
+    console.log(inputDishEl.value);
 }
 const ul = document.getElementById("dishes");
 const inputDishEl = document.getElementById("userinput");
 const addDishEl = document.querySelector("#adddish");
-addDishEl.addEventListener("click", addDish);
+addDishEl.addEventListener("click", addDish)
 
 document.getElementById("dishes").innerHTML += "<li>"
+
+
+
+
+
