3ba96078de90c4f6b0ff68cadbc87d1f3cc1c2ab
mailet01 <111491293+mailet01@users.noreply.github.com>
2022-12-05 15:44:14 +0100

    lägga till och ta bort element

--- dishes.js
+++ dishes.js
@@ -1,14 +1,37 @@
 function addDish(evt) {
     console.log("hello");
-    inputDishEl.value;
-    console.log(inputDishEl.value);
+    const dish = inputDishEl.value;
+    console.log(dish)
+    if (dish.length > 0) {
+        const li = document.createElement("li");
+        li.textContent = dish;
+        // skapa en ta bort knapp
+        const button = document.createElement("button");
+        button.textContent = "delete"
+
+
+        button.addEventListener("click", addDish);
+
+        button.addEventListener("click", (evt) => {
+            evt.target.parentElement.remove();
+        })
+
+
+        ul.appendChild(li);
+        inputDishEl.value = "";
+        li.appendChild(button)
+    } else {
+        inputDishEl.setAttribute("placeholder", "you have to write a dish")
+    }
+
+
 }
 const ul = document.getElementById("dishes");
 const inputDishEl = document.getElementById("userinput");
-const addDishEl = document.querySelector("#adddish");
+const addDishEl = document.querySelector("#addDish");
 addDishEl.addEventListener("click", addDish)
 
-document.getElementById("dishes").innerHTML += "<li>"
+
 
 
 
